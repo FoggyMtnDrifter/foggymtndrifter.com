e4990047ec3ec650b67972c43fd6c7aab2dd03f0
Michael Kinder <michael@foggymtndrifter.com>
2025-05-26 23:19:52 -0400

    fix api endpoint

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { EmbyService } from "../../lib/server/emby";
 
+export const prerender = false;
+
 export const GET: APIRoute = async () => {
   try {
     const embyService = new EmbyService();
